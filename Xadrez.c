2ea26335bd8e6e84933d822989290d27a1d70c8c
MayconGTR77 <maycongutierrez18@gmail.com>
2025-06-26 01:11:52 +0000

    bispo e torre

---
+++ Xadrez.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+
+    int bispo = 0, torre = 0, rainha = 0;
+
+    for  (bispo = 0; bispo < 5; bispo++){
+        printf("CIMA DIREITA\n");
+        
+    }
+
+    printf("\n");
+
+    while (torre < 5) {
+        printf("DIREITA\n");
+        torre++;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
